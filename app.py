bd0a723993ed96cb1b507492125def54b57b8354
cckhiani <cckhiani@gmail.com>
2025-02-16 18:37:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 def home():
  return 'My home page'
 
-@app.errorhandler(404)
+@app.route("/about")
+def about():
+    return render_template('about.html')@app.errorhandler(404)
+ 
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
